f6e3a5e17b7287540da29bec88753fd5c54a6a0d
ShockTohp <shocktohp@shocktohp.xyz>
2024-09-03 21:13:25 -0400

    Remove uneeded depencies

--- internal/util/utility.go
+++ internal/util/utility.go
@@ -2,26 +2,10 @@ package util
 
 import (
 	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func CheckErr(e error) {
 	if e != nil {
 		log.Fatal(e)
 	}
-}
-
-func CheckApiErr(e error, ctx *gin.Context) bool {
-	if e != nil {
-		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": e.Error()})
-		return true
-	}
-	return false
-}
-
-func ApplyCorsHeaders(ctx *gin.Context) {
-	ctx.Header("Access-Control-Allow-Origin", "*")
-	ctx.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers, content-type")
 }
